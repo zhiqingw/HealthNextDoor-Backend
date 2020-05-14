9f1b507721c68bd6ac7dcdb570674482d89f51ee
zhiqingw <zhiqingw@student.unimelb.edu.au>
2020-05-14 15:14:00 +0800

    cors

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var session = require('express-session');
 
 const app = express();
 require("./models");
+const cors = require('cors');
+
+app.use(cors());
 // use the body-parser middleware, which parses request bodies into req.body
 // support parsing of json
 app.use(session({
