88b824ad25344d8ee16ef05b208cba8b9269e273
zhiqingw <zhiqingw@student.unimelb.edu.au>
2020-06-05 19:03:52 +0800

    nodemodule

--- test/controllers/author.controller.spec.js
+++ test/controllers/author.controller.spec.js
@@ -1,42 +1,16 @@
 var sinon = require('sinon');
 var expect = require('chai').expect;
-var assert = require('chai').assert;
-var should = require('chai').should();
-
+const app = require('../../app');
+const mongoose = require("mongoose");
 var findCaregiverController =  require('../../controllers/findCaregiverController');
 
 
-describe('findCaregiverController', function () {
-    // Below, we are going to test HTTP functions, so we need to create fake request and respond object!
-
-    const mockResponse = (fake) => {
-        return {
-            send: fake
-        };
-    }
-
-    // this is just example how you can design the fake request, you can also add header property if your website needs one!
-    // I'm not even going to use any of these stuff inside request
-    const mockRequest = (session, body) => ({
-        session,
-        body,
-    });
-
+describe('unit test', function () {
     // I just want to remind that using chai is easier to read
-    describe('getAllAuthor', function() {
+    describe('add caregiver test', function() {
         //Checking length with style~, isntead of assert.equal(res.json.length, 2)
-        it("Author should have id, first_name, and last_name", function(){
-            const fake = sinon.fake();
-            const req = mockRequest({},{});
-            const res = mockResponse(fake);
-
-            findCaregiverController.getAllCaregivers(req,res)
-            const result = fake.lastArg;
-
-            result.forEach(element => {
-                expect(element).to.have.property('username');//check one with chai
-
-            });
+        it("add caregiver", function(){
+            let nesCaregiver = {};
         })
     })
 });
